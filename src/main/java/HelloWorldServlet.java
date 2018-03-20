c60b909af385c41887bef78dd8b970491926365f
coopmostcurious <hanke31314@gmail.com>
2018-03-20 14:58:02 -0500

    finished with servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        handleRequest(req, res);
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        handleRequest(req, res);
+    }
+    protected void handleRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String param1 = req.getParameter("name");
+        PrintWriter out = res.getWriter();
+        res.setContentType("text/html");
+        out.write("<h1>Hello, " + param1 + "!!");
+        out.close();
     }
 }
