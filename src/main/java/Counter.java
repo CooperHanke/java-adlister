5ab71fb44b4b78d78e45f7cbaf5df4297a6a1c1c
coopmostcurious <hanke31314@gmail.com>
2018-03-20 15:46:32 -0500

    finished the bonuses, able to reset by passing reset=true

---
+++ src/main/java/Counter.java
@@ -0,0 +1,21 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Counter", urlPatterns = "/count")
+public class Counter extends HttpServlet {
+    private int counter = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String parameter = request.getParameter("reset");
+        if (parameter == null || parameter.isEmpty()) {
+            counter += 1;
+            response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+        } else {
+            response.getWriter().println("<h1>The count has now been reset!</h1>");
+            counter = 0;
+        }
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,23 +3,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Enumeration;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        handleRequest(req, res);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        handleRequest(request, response);
     }
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        handleRequest(req, res);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        handleRequest(request, response);
     }
-    protected void handleRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        String param1 = req.getParameter("name");
-        PrintWriter out = res.getWriter();
-        res.setContentType("text/html");
-        out.write("<h1>Hello, " + param1 + "!!");
-        out.close();
+    protected void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String param = request.getParameter("name");
+        response.setContentType("text/html");
+        response.getWriter().write("<h1>Hello there, " + param + "! Thanks for visiting!");
     }
 }
