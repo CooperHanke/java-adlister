1339c5c6fabbf3a75f6a9ec432c5b597309ce120
Cooper Hanke <cooper.hanke@gmail.com>
2018-03-29 14:20:49 -0500

    now the profile carries a session when we go to it and we have a way to see the name in a styled way

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -14,11 +15,15 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
-
-        if (!validAttempt) {
+        boolean validAttempt = username.equals("admin") && password.equals("password") ||
+                username.equals("cooper") && password.equals("password");
+        if (validAttempt) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user",username.substring(0,1).toUpperCase() +
+            username.substring(1));
+            response.sendRedirect("/profile");
+        } else {
             response.sendRedirect("/login");
         }
-        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
