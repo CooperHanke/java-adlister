4498fae4fd656d99daa2c57ffb2f2cf687b31a49
Cooper Hanke <cooper.hanke@gmail.com>
2018-04-03 14:49:56 -0500

    able to render ads from database

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -6,10 +6,46 @@ import com.mysql.cj.jdbc.Driver;
 
 public class MySQLAdsDao implements Ads {
     private static Connection connection = null;
-    private static Config config = new Config();
-    private List<Ad> ads;
 
-//    public static void main(String[] args) {
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+            if (connection != null) {
+                System.out.println("Connected to database");
+            }
+        } catch (SQLException e) {
+            e.getSQLState();
+        }
+    }
+
+        @Override
+        public List<Ad> all() {
+            List<Ad> ads = new ArrayList<>();
+            try {
+                Statement stmt = connection.createStatement();
+                ResultSet results = stmt.executeQuery("SELECT * FROM ads");
+                while (results.next()) {
+                    Ad ad = new Ad(
+                            results.getLong("id"),
+                            results.getLong("user_id"),
+                            results.getString("title"),
+                            results.getString("description")
+                    );
+                    System.out.println("added item");
+                    ads.add(ad);
+                }
+            } catch (SQLException e) {
+                e.getSQLState();
+            }
+            return ads;
+        }
+
+    //    public static void main(String[] args) {
 //        try {
 //            DriverManager.registerDriver(new Driver());
 //            connection = DriverManager.getConnection(
@@ -28,35 +64,35 @@ public class MySQLAdsDao implements Ads {
 //        }
 //    }
 
-    @Override
-    public List<Ad> all() {
-        if (ads == null) {
-            try {
-                DriverManager.registerDriver(new Driver());
-                connection = DriverManager.getConnection(
-                        config.getUrl(),
-                        config.getUsername(),
-                        config.getPassword()
-                );
-                Statement stmt = connection.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM adlister_db.ads");
-                while (rs.next()) {
-                    long id = rs.getLong("id");
-                    long userid = rs.getLong("userid");
-                    String title = rs.getString("title");
-                    String des = rs.getString("description");
-                    Ad ad = new Ad (id, userid, title, des);
-                    ads.add(ad);
-                }
-            } catch (SQLException e) {
-                e.getSQLState();
-            }
-        }
-        return ads;
-    }
 
-    @Override
-    public Long insert(Ad ad) {
-        return null;
-    }
+
+//        if (ads == null) {
+//            try {
+//                DriverManager.registerDriver(new Driver());
+//                connection = DriverManager.getConnection(
+//                        config.getUrl(),
+//                        config.getUsername(),
+//                        config.getPassword()
+//                );
+//                Statement stmt = connection.createStatement();
+//                ResultSet rs = stmt.executeQuery("SELECT * FROM adlister_db.ads");
+//                while (rs.next()) {
+//                    long id = rs.getLong("id");
+//                    long userid = rs.getLong("userid");
+//                    String title = rs.getString("title");
+//                    String des = rs.getString("description");
+//                    Ad ad = new Ad (id, userid, title, des);
+//                    ads.add(ad);
+//                }
+//            } catch (SQLException e) {
+//                e.getSQLState();
+//            }
+//        }
+//        return ads;
+//    }
+
+//    @Override
+//    public Long insert(Ad ad) {
+//        return null;
+//    }
 }
