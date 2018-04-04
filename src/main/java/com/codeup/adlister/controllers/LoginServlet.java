b6773979fd6b1121f053110ab0a9bd9af3cf885a
Cooper Hanke <cooper.hanke@gmail.com>
2018-04-04 15:36:08 -0500

    enabled functionality to check a password

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,11 +1,16 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.Users;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -20,15 +25,29 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-
+        User user = null;
+        boolean validAttempt = false;
         // TODO: find a record in your database that matches the submitted password
         // TODO: make sure we find a user with that username
+        try {
+            user = DaoFactory.getUsersDao().findByUsername(username);
+            if (user != null) {
+                System.out.println("Found the user!!");
+                if (user.getPassword().equals(password)) {
+                    System.out.println("Passwords match!");
+                    validAttempt = true;
+                }
+            } else {
+                System.out.println("Did not find them");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         // TODO: check the submitted password against what you have in your database
-        boolean validAttempt = false;
 
         if (validAttempt) {
             // TODO: store the logged in user object in the session, instead of just the username
-            request.getSession().setAttribute("user", username);
+            request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,51 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.List;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) throws SQLException {
+        String sql = "SELECT * FROM users WHERE username = ?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setString(1, username);
+        stmt.executeQuery();
+        ResultSet rs = stmt.getResultSet();
+        if (rs.next()) {
+            return extractUser(rs);
+        } else { return null; }
+    }
+
+    private User extractUser(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
+        );
+    }
+
+    @Override
+    public Long insert(User user) {
+        return null;
+    }
+}
