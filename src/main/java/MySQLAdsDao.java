bab8c719b2de8f30e6e8355ab4557c4ba16c1409
Cooper Hanke <cooper.hanke@gmail.com>
2018-04-03 21:02:34 -0500

    finished with exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -7,7 +7,7 @@ import com.mysql.cj.jdbc.Driver;
 public class MySQLAdsDao implements Ads {
     private static Connection connection = null;
 
-    public MySQLAdsDao(Config config) {
+    protected MySQLAdsDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
@@ -23,76 +23,45 @@ public class MySQLAdsDao implements Ads {
         }
     }
 
-        @Override
-        public List<Ad> all() {
-            List<Ad> ads = new ArrayList<>();
-            try {
-                Statement stmt = connection.createStatement();
-                ResultSet results = stmt.executeQuery("SELECT * FROM ads");
-                while (results.next()) {
-                    Ad ad = new Ad(
-                            results.getLong("id"),
-                            results.getLong("user_id"),
-                            results.getString("title"),
-                            results.getString("description")
-                    );
-                    System.out.println("added item");
-                    ads.add(ad);
-                }
-            } catch (SQLException e) {
-                e.getSQLState();
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet results = stmt.executeQuery("SELECT * FROM ads");
+            while (results.next()) {
+                Ad ad = new Ad(
+                        results.getLong("id"),
+                        results.getLong("user_id"),
+                        results.getString("title"),
+                        results.getString("description")
+                );
+                System.out.println("added item");
+                ads.add(ad);
             }
-            return ads;
+        } catch (SQLException e) {
+            e.getSQLState();
         }
+        return ads;
+    }
 
-    //    public static void main(String[] args) {
-//        try {
-//            DriverManager.registerDriver(new Driver());
-//            connection = DriverManager.getConnection(
-//                    config.getUrl(),
-//                    config.getUsername(),
-//                    config.getPassword()
-//            );
-//            Statement stmt = connection.createStatement();
-//            ResultSet rs = stmt.executeQuery("select 1 + 1 as result");
-//            while (rs.next()) {
-//                System.out.println(rs.getInt("result"));
-//            }
-//            System.out.println("would be all");
-//        } catch (SQLException e) {
-//            e.getErrorCode();
-//        }
-//    }
-
-
-
-//        if (ads == null) {
-//            try {
-//                DriverManager.registerDriver(new Driver());
-//                connection = DriverManager.getConnection(
-//                        config.getUrl(),
-//                        config.getUsername(),
-//                        config.getPassword()
-//                );
-//                Statement stmt = connection.createStatement();
-//                ResultSet rs = stmt.executeQuery("SELECT * FROM adlister_db.ads");
-//                while (rs.next()) {
-//                    long id = rs.getLong("id");
-//                    long userid = rs.getLong("userid");
-//                    String title = rs.getString("title");
-//                    String des = rs.getString("description");
-//                    Ad ad = new Ad (id, userid, title, des);
-//                    ads.add(ad);
-//                }
-//            } catch (SQLException e) {
-//                e.getSQLState();
-//            }
-//        }
-//        return ads;
-//    }
-
-//    @Override
-//    public Long insert(Ad ad) {
-//        return null;
-//    }
+    @Override
+    public Long insert(Ad ad) {
+        String query = "INSERT INTO ads (user_id, title, description) VALUES " +
+                "(" + ad.getUserId() + ",'" + ad.getTitle() + "', '" + ad.getDescription() + "')";
+        Long id = null;
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            System.out.println(rs);
+            if (rs.next()) {
+                id = rs.getLong(1);
+                return id;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("ERROR inserting ad ", e);
+        }
+    return null;
+    }
 }
